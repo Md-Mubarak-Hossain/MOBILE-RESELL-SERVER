c0d1a1dbb16549bf9c0be922cf379bedf56cd094
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-11-24 09:20:54 +0600

    server check complete with client

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 // const package = require('./package.json')
 const port = process.env.PORT || 5000;
@@ -41,6 +41,47 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result)
         })
+        app.get('/package/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await mobileCollection.findOne(query);
+            res.send(result)
+        })
+
+        app.post('/package', async (req, res) => {
+            const user = req.body;
+            const result = await mobileCollection.insertOne(user);
+            console.log(result);
+            res.send(result);
+        })
+
+        app.put('/package/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id);
+            const filter = {
+                _id: ObjectId(id)
+            };
+            const package = req.body;
+            console.log(package);
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    name: package.name,
+                    version: package.version
+                }
+            }
+            const result = await mobileCollection.updateOne(filter, updateDoc, options);
+            res.send(result);
+            console.log(result);
+        })
+
+        app.delete('/package/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await mobileCollection.deleteOne(query);
+            res.send(result);
+        })
+
         app.get('/addData', async (req, res) => {
             const query = {};
             const cursor = newCollection.find(query);
@@ -48,16 +89,6 @@ async function run() {
             res.send(result)
         })
 
-        app.post('/addData', async (req, res) => {
-
-            const doc = {
-                title: "Mubarak data",
-                content: "Data insert check",
-            }
-            const result = await newCollection.insertOne(doc);
-            console.log(result);
-            res.send(result);
-        })
 
 
     } finally {
