6ef040dc75846b00ddc05fb2d671644c8ae242d5
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-11-25 15:51:44 +0600

    mobile data added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-// const package = require('./package.json')
+// const mobile = require('./mobile.json')
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -11,8 +11,8 @@ require('dotenv').config();
 app.use(cors())
 app.use(express.json())
 
-// app.get('/package', (req, res) => {
-//     res.send(package);
+// app.get('/mobile', (req, res) => {
+//     res.send(mobile);
 // })
 app.get('/', (req, res) => {
     res.send('mobile re-seller server running');
@@ -32,42 +32,68 @@ async function run() {
     try {
         const database = client.db("resellerDatabase");
         const mobileCollection = database.collection("mobile");
-        const newCollection = database.collection("test");
+        const blogCollection = database.collection("blog");
+        const faqCollection = database.collection("faqAns");
         // Query for a movie that has the title 'The Room'
-
-        app.get('/package', async (req, res) => {
+        // blog data
+        app.get('/blog', async (req, res) => {
+            const query = {};
+            const cursor = blogCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+        // end blog
+        // faq data
+        app.get('/faqAns', async (req, res) => {
+            const query = {};
+            const cursor = faqCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+        // faq end
+        app.get('/mobile', async (req, res) => {
             const query = {};
             const cursor = mobileCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
         })
-        app.get('/package/:id', async (req, res) => {
+        app.get('/mobile/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await mobileCollection.findOne(query);
             res.send(result)
         })
 
-        app.post('/package', async (req, res) => {
+        app.post('/mobile', async (req, res) => {
             const user = req.body;
             const result = await mobileCollection.insertOne(user);
             console.log(result);
             res.send(result);
         })
 
-        app.put('/package/:id', async (req, res) => {
+        app.put('/mobile/:id', async (req, res) => {
             const id = req.params.id;
             console.log(id);
             const filter = {
                 _id: ObjectId(id)
             };
-            const package = req.body;
-            console.log(package);
+            const mobile = req.body;
+            console.log(mobile);
             const options = { upsert: true };
             const updateDoc = {
                 $set: {
-                    name: package.name,
-                    version: package.version
+                    name: mobile.name,
+                    brand: mobile.brand,
+                    ram: mobile.ram,
+                    camera: mobile.camera,
+                    useTime: mobile.useTime,
+                    resalePrice: mobile.resalePrice,
+                    category: mobile.category,
+                    battery,
+                    price,
+                    location,
+                    picture,
+                    seller
                 }
             }
             const result = await mobileCollection.updateOne(filter, updateDoc, options);
@@ -75,7 +101,7 @@ async function run() {
             console.log(result);
         })
 
-        app.delete('/package/:id', async (req, res) => {
+        app.delete('/mobile/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await mobileCollection.deleteOne(query);
