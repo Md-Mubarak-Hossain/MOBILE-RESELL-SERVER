2319e04f344d6482769d08b91ee9e03eb9260670
Md-Mubarak-Hossain <hossainmubarak477@gmail.com>
2022-11-30 23:45:31 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
 const app = express();
 // const mobile = require('./mobile.json')
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
-
 // middle ware
 app.use(cors())
 app.use(express.json())
 
-// app.get('/mobile', (req, res) => {
-//     res.send(mobile);
-// })
 app.get('/', (req, res) => {
     res.send('mobile re-seller server running');
 })
-
 app.listen(port, () => {
     console.log(`The server is mobile re-seller and the port : ${port}`);
 })
@@ -28,6 +24,22 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 console.log(uri)
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         const database = client.db("resellerDatabase");
@@ -38,7 +50,15 @@ async function run() {
         const userCollection = database.collection("user");
         const sellerCollection = database.collection("seller");
         const wishCollection = database.collection("addWishList");
-        // Query for a movie that has the title 'The Room'
+        /*..............................
+               JWT api token
+        ................................*/
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10h' })
+
+            res.send({ token })
+        })
         // admin email password
         app.get('/admin', async (req, res) => {
             const query = {};
@@ -168,6 +188,29 @@ async function run() {
         // faq end
         app.get('/mobile', async (req, res) => {
             const query = {};
+            const cursor = mobileCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+        // app.get('/mobileWithEmail', verifyJWT, async (req, res) => {
+        //     const decoded = req.decoded;
+        //     if (decoded.email !== req.query.email) {
+        //         return res.status(403).send({ message: 'unauthorized access' })
+        //     }
+        //     let query = {}
+        //     if (req.query.email) {
+        //         query = { email: req.query.email }
+        //     }
+        //     const cursor = mobileCollection.find(query)
+        //     const result = await cursor.toArray();
+        //     res.send(result);
+        // });
+
+        app.get('/mobileWithEmail', async (req, res) => {
+            let query = {}
+            if (req.query.email) {
+                query = { email: req.query.email }
+            }
             const cursor = mobileCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
